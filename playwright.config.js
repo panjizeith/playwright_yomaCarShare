2a25e54818842088bfcf68178ec241933837dfc6
PI <panji.indrajit@gmail.com>
2025-07-13 20:21:46 +0700

    fix report allure

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['allure-playwright'],['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -45,10 +45,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+ //   {
+ //     name: 'webkit',
+ //     use: { ...devices['Desktop Safari'] },
+ //   },
 
     /* Test against mobile viewports. */
     // {
